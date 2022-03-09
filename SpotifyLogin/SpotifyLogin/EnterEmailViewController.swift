d8776fa4bc6d9607e6b66060127564e84e3a12e7
Junghun Park <tacohun21@gmail.com>
2022-03-09 22:42:23 +0900

    이메일 로그인/로그아웃

--- SpotifyLogin/SpotifyLogin/EnterEmailViewController.swift
+++ SpotifyLogin/SpotifyLogin/EnterEmailViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class EnterEmailViewController: UIViewController{
     
@@ -29,6 +30,46 @@ class EnterEmailViewController: UIViewController{
         navigationController?.navigationBar.isHidden = false
     }
     @IBAction func nextButtonTapped(_ sender: UIButton) {
+        //Firebase 이메일/비밀번호 인증
+        let email = emailTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        
+        //신규 사용자 생성
+        Auth.auth().createUser(withEmail: email, password: password){ [weak self] authResult, error in
+            guard let self = self else {return}
+            
+            if let error = error {
+                let code = (error as NSError).code
+                switch code {
+                case 17007: //이미 가입한 계정일 때
+                    //로그인하기
+                    self.loginUser(withEmail: email, password: password)
+                default:
+                    self.errorMessageLabel.text = error.localizedDescription
+                }
+            }else {
+                self.showMainViewController()
+            }
+        }
+    }
+    
+    private func showMainViewController(){
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        let mainViewController = storyboard.instantiateViewController(identifier: "MainViewController")
+        mainViewController.modalPresentationStyle = .fullScreen
+        navigationController?.show(mainViewController,sender: nil)
+    }
+    
+    private func loginUser(withEmail email:String, password: String){
+        Auth.auth().signIn(withEmail: email, password: password) {[weak self] _, error in
+            guard let self = self else {return}
+            
+            if let error = error {
+                self.errorMessageLabel.text = error.localizedDescription
+            }else {
+                self.showMainViewController()
+            }
+        }
     }
 }
 extension EnterEmailViewController: UITextFieldDelegate {
